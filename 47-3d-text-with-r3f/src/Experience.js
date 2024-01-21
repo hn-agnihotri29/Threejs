e04bcb4ba6a1a11294edaa2a32a16594ecdffb0d
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2024-01-21 22:52:55 +0530

    r3f 3d text

---
+++ 47-3d-text-with-r3f/src/Experience.js
@@ -0,0 +1,18 @@
+import { Text3D,OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+
+export default function Experience()
+{
+    return <>
+
+        <Perf position="top-left" />
+
+        <OrbitControls makeDefault />
+
+        <mesh scale={ 1.5 }>
+            <boxGeometry />
+            <meshNormalMaterial />
+        </mesh>
+
+    </>
+}
