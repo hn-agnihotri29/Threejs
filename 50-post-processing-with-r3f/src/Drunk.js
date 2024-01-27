a260f29c2f9477f6e21225dccbda71d23fc04899
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2024-01-27 23:23:37 +0530

    r3f post processing

---
+++ 50-post-processing-with-r3f/src/Drunk.js
@@ -0,0 +1,11 @@
+import DrunkEffect from "./DrunkEffect.js"
+import { forwardRef } from "react"
+
+const Drunk = (props, ref) => {
+
+    const effect = new DrunkEffect(props)
+
+    return <primitive  ref={ref} object={effect} />
+}
+
+export default forwardRef(Drunk)
